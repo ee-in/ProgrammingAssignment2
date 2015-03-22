04f4549c4fcdaeccd436b3322fd400888b6ec146
yan makryche <ian.m.ritchie@gmail.com>
2015-03-22 18:09:03 -0400

    ProgrammingAssignment2_IMR
    Updates to file <<cachematrix.R>> for R Programming on courser,
    ‘ProgrammingAssignment2’

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# # This function creates a special "matrix" object
+# # that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      # # creates null 'inv', stores 'x' as 'y' in the global environment, 
+      # # also makes 'inv' global
+      inv <- NULL
+      set <- function(y) {
+            x <<- y
+            inv <<- NULL
+      }
+      # # retrieves the function storing x, solves/inverts, caches in inv in parent enviro
+      get <- function() x
+      setinv <- function(solve) inv <<- solve
+      getinv <- function() inv
+      list(set = set, get = get,
+           setinv = setinv,
+           getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+# # Computes the inverse of the special "matrix" returned by `makeCacheMatrix` 
+# # If the inverse has already been calculated (and the matrix has not changed),
+# # strthen `cacheSolve` should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      # # stores solved/inverse of 'x' in previously NULL 'inv'
+      inv <- x$getinv()
+      
+      # # checks if there is already cached data/vector, in this case the matrix
+      if(!is.null(inv)) {
+            message("getting cached data")
+            return(inv)
+      }
+      
+      # # 'if' statement is not fullfiled, gets 'x', stores it as data, inverts, returns
+      message("calculating data")
+      data <- x$get()
+      inv <- solve(data, ...)
+      x$setinv(inv)
+      inv
 }
